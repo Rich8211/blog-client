43db9a6ae5206d2376a33d831dee56c39188eb7e
Rich8211 <richlee821@gmail.com>
2021-05-07 21:19:35 -0400

    Add reload to infinite scroll

--- src/Pages/Posts/Posts.jsx
+++ src/Pages/Posts/Posts.jsx
@@ -11,25 +11,41 @@ const Posts = () => {
     const smallMatches = useMediaQuery('(max-width:794px)');
 
     const [posts, setPosts] = useState([]);
-    const [skip, setskip] = useState(0);
+    const [skip, setSkip] = useState(0);
     const [hasMore, setHasMore] = useState(true);
     const [count, setCount] = useState(0);
     const [isLoading, setIsLoading] = useState(false);
 
-    const observer = useRef();
+    const lastObserver = useRef();
     const lastPostRef = useCallback(node => {
         if (isLoading) return;
-        if (observer.current) observer.current.disconnect()
-        observer.current = new IntersectionObserver(entries => {
+        if (lastObserver.current) lastObserver.current.disconnect();
+        lastObserver.current = new IntersectionObserver(entries => {
             if (entries[0].isIntersecting && hasMore) {
-                if ( skip < count) {
-                    setskip(prevSkip => prevSkip + 12);
+                if ( skip < count ) {
+                    setSkip(prev => prev + 12);
                 }
                 else setHasMore(false); 
             }
         });
-        if (node) observer.current.observe(node)
+        if (node) lastObserver.current.observe(node);
     });
+
+
+    const firstObserver = useRef();
+    const firstPostRef = useCallback(
+        node => { 
+            console.log(node)
+            if (firstObserver.current) firstObserver.current.disconnect();
+            firstObserver.current = new IntersectionObserver(entries => {
+                if (entries[0].isIntersecting ) {
+                    setSkip(0)
+                }
+            });
+        if (node) firstObserver.current.observe(node);
+    });
+
+    
 
     useEffect(() => {
         const getPosts = async () => {
@@ -38,27 +54,30 @@ const Posts = () => {
             const countRes = await axios.get('http://localhost:5000/posts/info/count');
             setCount(countRes.data);
         }
-        getPosts();
-    },[]);
-
-    useEffect(() => {
-        if (skip === 0) return;
+        if (skip === 0) 
+            {
+                getPosts();
+                return;
+            }
         const getPostsPaginated = async (skip,limit) => {
             setIsLoading(true);
             const postRes = await axios.get(`http://localhost:5000/posts/?limit=${limit}&skip=${skip}`);
             if (postRes.data.length > 0) setPosts(prevPosts => [...prevPosts,...postRes.data]);
             setIsLoading(false);
         }
-        getPostsPaginated(skip,12)
+        getPostsPaginated(skip,12);
     },[skip]);
+
 
     return (
         <div className={matches ? "posts-page" : smallMatches ? "posts-page-mobile": "posts-page-tablet"}>
             <div className={matches? "post-list" : "post-list-mobile" }>
                 {posts.length > 0 && posts.map((post,i) => {
                     if (posts.length === i + 1) 
-                        return <PostListItem innerRef={lastPostRef} key={post._id} post={post}/>
-                    else return <PostListItem key={post._id} post={post}/>   
+                        return <PostListItem innerRef={lastPostRef} key={post._id} post={post}/>  
+                    else if (i === 0)
+                        return <PostListItem innerRef={firstPostRef} key={post._id} post={post} />
+                    else return <PostListItem key={post._id} post={post}/>    
                     }
                 )}
             </div>
